70680247291f8787049f8379402a89b630689680
snapoles <shelisan@msn.com>
2024-06-10 17:22:54 -0700

    updated function get BooksBorrowedCount in home.js file

--- public/src/home.js
+++ public/src/home.js
@@ -12,6 +12,7 @@ function getBooksBorrowedCount(books) {
   // YOUR SOLUTION HERE
   // Hint: You can use the [`filter()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter) method here. 
   // If you get stuck, feel free to take a look at this repl.it: https://replit.com/@thinkful/getBooksBorrowedCount#index.js
+  return books.filter((book) => !book.borrows[0].returned).length;
 }
 
 // Tbis is a helper function that's called by other functions inside this file. You don't have to edit it.
